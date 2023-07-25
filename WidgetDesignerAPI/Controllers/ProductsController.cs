d65f0581cab8425f351f0f5482d0bf9f812d5750
mukul rai <mukulrai141966@gmail.com>
2023-07-25 18:11:25 +0530

    Add project files.

---
+++ WidgetDesignerAPI/Controllers/ProductsController.cs
@@ -0,0 +1,84 @@
+﻿﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WidgetDesignerAPI.API.Data;
+using WidgetDesignerAPI.Models;
+
+namespace WidgetDesignerAPI.API.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly WidgetDesignerAPIDbContext _widgetDesignerAPIDbContext;
+
+        public ProductsController(WidgetDesignerAPIDbContext widgetDesignerAPIDbContext)
+        {
+            this._widgetDesignerAPIDbContext = widgetDesignerAPIDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var products = await _widgetDesignerAPIDbContext.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct([FromBody] Product product)
+        {
+            product.Id = Guid.NewGuid();
+
+            await _widgetDesignerAPIDbContext.Products.AddAsync(product);
+            await _widgetDesignerAPIDbContext.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetProduct(Guid id)
+        {
+            var product = await _widgetDesignerAPIDbContext.Products.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(product == null)
+                return NotFound();
+
+            return Ok(product);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateProduct([FromRoute] Guid id, Product updateProductRequest)
+        {
+            var product = await _widgetDesignerAPIDbContext.Products.FindAsync(id);
+
+            if (product == null)
+                return NotFound();
+
+            product.Name = updateProductRequest.Name;
+            product.Type = updateProductRequest.Type;
+            product.Color = updateProductRequest.Color;
+            product.Price = updateProductRequest.Price;
+
+            await _widgetDesignerAPIDbContext.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteProduct(Guid id)
+        {
+            var product = await _widgetDesignerAPIDbContext.Products.FindAsync(id);
+
+            if (product == null)
+                return NotFound();
+
+            _widgetDesignerAPIDbContext.Products.Remove(product);
+            await _widgetDesignerAPIDbContext.SaveChangesAsync();
+
+            return Ok(product);
+        }
+    }
+}
---
+++ WidgetDesignerAPI/Data/WidgetDesignerDbContext.cs
@@ -0,0 +1,13 @@
+﻿
+﻿using Microsoft.EntityFrameworkCore;
+using WidgetDesignerAPI.Models;
+
+namespace WidgetDesignerAPI.API.Data
+{
+    public class WidgetDesignerAPIDbContext : DbContext
+    {
+        public WidgetDesignerAPIDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
