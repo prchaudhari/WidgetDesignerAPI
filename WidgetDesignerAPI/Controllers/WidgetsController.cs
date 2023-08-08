2f24079d5173ab8101c60d9938df4617761e4bd6
prchaudhari <chaudhari.pallavi@gmail.com>
2023-08-08 19:21:36 +0530

    Image upload changes in API

--- WidgetDesignerAPI/Controllers/WidgetsController.cs
+++ WidgetDesignerAPI/Controllers/WidgetsController.cs
@@ -1,7 +1,10 @@
 ﻿﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using WidgetDesignerAPI.API.Data;
 using WidgetDesignerAPI.Models;
+using static System.Net.Mime.MediaTypeNames;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace WidgetDesignerAPI.API.Controllers
 {
@@ -10,10 +13,13 @@ namespace WidgetDesignerAPI.API.Controllers
     public class WidgetsController : Controller
     {
         private readonly WidgetDesignerAPIDbContext _widgetDesignerAPIDbContext;
-
-        public WidgetsController(WidgetDesignerAPIDbContext widgetDesignerAPIDbContext)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private IHostingEnvironment Environment;
+        public WidgetsController(WidgetDesignerAPIDbContext widgetDesignerAPIDbContext, IWebHostEnvironment webHostEnvironment, Microsoft.AspNetCore.Hosting.IHostingEnvironment environment)
         {
             this._widgetDesignerAPIDbContext = widgetDesignerAPIDbContext;
+            _webHostEnvironment = webHostEnvironment;
+            Environment = environment;
         }
 
         [HttpGet]
@@ -24,16 +30,16 @@ namespace WidgetDesignerAPI.API.Controllers
             return Ok(widgets);
         }
 
-        [HttpPost]
-        public async Task<IActionResult> AddWidget([FromBody] Widgets widget)
-        {
-            //widget.Id = Guid.NewGuid();
+        //[HttpPost]
+        //public async Task<IActionResult> AddWidget([FromBody] Widgets widget)
+        //{
+        //    //widget.Id = Guid.NewGuid();
 
-            await _widgetDesignerAPIDbContext.Widgets.AddAsync(widget);
-            await _widgetDesignerAPIDbContext.SaveChangesAsync();
+        //    await _widgetDesignerAPIDbContext.Widgets.AddAsync(widget);
+        //    await _widgetDesignerAPIDbContext.SaveChangesAsync();
 
-            return Ok(widget);
-        }
+        //    return Ok(widget);
+        //}
 
         [HttpGet]
         [Route("{id:int}")]
@@ -79,8 +85,59 @@ namespace WidgetDesignerAPI.API.Controllers
 
             _widgetDesignerAPIDbContext.Widgets.Remove(widget);
             await _widgetDesignerAPIDbContext.SaveChangesAsync();
-
             return Ok(widget);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadImage([FromForm] WidgetModel imageModel)
+        {
+            // Handle the image upload and store it in the project folder
+            string imagePath = SaveImage(imageModel.WidgetIconUrl);
+
+            // Save the image path and other information into the database
+            var newImage = new Widgets
+            {
+                WidgetIconUrl = imagePath,
+                WidgetName = imageModel.WidgetName,
+                Description = imageModel.Description,
+                WidgetHtml = imageModel.WidgetHtml,
+                Width = imageModel.Width,
+                Height = imageModel.Height,
+                DataSourceJson = imageModel.DataSourceJson
+                
+                // Set other fields as needed
+            };
+
+            await _widgetDesignerAPIDbContext.Widgets.AddAsync(newImage);
+            await _widgetDesignerAPIDbContext.SaveChangesAsync();
+
+            return Ok(newImage);
+        }
+
+        private string SaveImage(IFormFile imageFile)
+        {
+            if (imageFile == null)
+            {
+                return "";
+            }
+            string uploadsFolder = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName,"uploads");
+           // var uploadsFolder = Path.Combine(this.Environment.WebRootPath, "uploads");
+            if (!Directory.Exists(uploadsFolder))
+            {
+                Directory.CreateDirectory(uploadsFolder);
+            }
+            var imageName = Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName);
+            var imagePath = Path.Combine(uploadsFolder, imageName);
+
+            using (var stream = new FileStream(imagePath, FileMode.Create))
+            {
+                imageFile.CopyTo(stream);
+            }
+
+            var imageUrl = Url.Content(Path.Combine("~/uploads", imageName));
+            return imageUrl;
+        }
+
+
     }
 }
---
+++ WidgetDesignerAPI/Models/WidgetModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WidgetDesignerAPI.Models
+{
+    public class WidgetModel
+    {
+        public int Id { get; set; }
+        public string WidgetName { get; set; }
+        public string Description { get; set; }
+        public string DataSourceJson { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string WidgetHtml { get; set; }      
+        public IFormFile WidgetIconUrl { get; set; }       
+        public string? WidgetCSSUrl { get; set; }
+        public string? WidgetCSS { get; set; }
+    }
+}
